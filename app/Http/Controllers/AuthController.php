7aad79e6f17af24aa8276aa27befcad519691169
Aditya Prasetya Kusnadi <58908825+AdityaPKusnadi@users.noreply.github.com>
2026-09-23 15:44:02 +0700

    End Point login dan juga update readme
    - perubahan readme untuk penjelasan dan screenshot hasil testing endpoint login
    - penambahan endpoint login
    - penambahan seeder login untuk testing karena belum ada crud

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        $user = User::where('email', $credentials['email'])->first();
+
+        if (! $user || ! Hash::check($credentials['password'], $user->password)) {
+            return response()->json([
+                'message' => 'Email atau password salah.',
+            ], 401);
+        }
+
+        return response()->json([
+            'token' => $user->createToken('auth_token')->plainTextToken,
+            'user' => $user,
+        ]);
+    }
+}
